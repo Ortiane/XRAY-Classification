a98399296fbe7af74d0d0a7ce9dad98712986f4b
Ortiane <dchang2@andrew.cmu.edu>
2020-04-18 00:51:55 -0400

    changed metric to monitor for ckpt

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def train(net, dataset, epochs, model_save_dir, logdir):
     log_dir = os.path.join(logdir, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
     mc = tf.keras.callbacks.ModelCheckpoint(
         model_save_dir + "model.h5",
-        monitor="loss",
+        monitor="val_binary_accuracy",
         mode="max",
         verbose=1,
         save_best_only=True,
